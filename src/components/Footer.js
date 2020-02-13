03d3f5c3e68d47c3c81bd326d526b989f94c0569
carloscasciano <carlos.casciano@gmail.com>
2020-02-13 18:32:02 -0300

    Add Footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,29 @@
  import React from 'react'
+ import GitHubIcon from '@material-ui/icons/GitHub'
+ import LinkedInIcon from '@material-ui/icons/LinkedIn'
+ import Grid from '@material-ui/core/Grid'
+ import Link from '@material-ui/core/Link'
+
 
  const Footer = () => {
      return (
          <>
-            <p>coded by carlos casciano - github - linkedin</p>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                <p>coded by carlos casciano</p>
+                <Link href="https://github.com/carloscasciano/">
+                    <GitHubIcon />
+                </Link>
+                <Link href="https://www.linkedin.com/in/carloscasciano/">
+                    <LinkedInIcon />
+                </Link>
+                
+            </Grid>
+            
          </>
      )
  }
