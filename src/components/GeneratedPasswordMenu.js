a505faa55e34c3bcea9e85395ca987f984302557
carloscasciano <carlos.casciano@gmail.com>
2020-02-13 17:42:14 -0300

    Add GeneratedPasswordMenu component style

--- src/components/GeneratedPasswordMenu.js
+++ src/components/GeneratedPasswordMenu.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import Textfield from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
+
 
 const GeneratedPasswordMenu = (props) => {
     return (
         <>
-            <input value={props.generatedPassword} readOnly></input>
-            <p>strength: {props.passwordStrength}</p>
-        </>
+            <Grid>
+                <Textfield 
+                    variant = "outlined"
+                    value = {props.generatedPassword} 
+                    helperText = {props.passwordStrength}
+                    readOnly 
+                />
+            </Grid>
+        </>           
     )
 }
 
