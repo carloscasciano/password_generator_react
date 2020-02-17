e1f70f47dfabbef0f4d24fa39d32753198c0e785
carloscasciano <carlos.casciano@gmail.com>
2020-02-17 11:27:20 -0300

    Add color text for password strength feature

--- src/components/GeneratedPasswordMenu.js
+++ src/components/GeneratedPasswordMenu.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import Textfield from '@material-ui/core/TextField'
 import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles'
 
+const getColor = (passwordStrength) => {
+    switch (passwordStrength) {
+        case 'risky' : return '#FF2E1D'
+        case 'weak' : return '#FF720F' 
+        case 'medium' : return '#E8A72C'
+        case 'strong' : return '#5BA324'
+        case 'very strong' : return '#144A0A'
+        default : return 'gray'
+    }
+}
 
 const GeneratedPasswordMenu = (props) => {
+
+    const currentColor = getColor(props.passwordStrength)
+    const useStyles = makeStyles({
+        root: {
+          color: currentColor
+        }
+      })
+    const classes = useStyles()
+
     return (
         <>
             <Grid
@@ -16,7 +36,8 @@ const GeneratedPasswordMenu = (props) => {
                     variant = "outlined"
                     value = {props.generatedPassword} 
                     helperText = {props.passwordStrength}
-                    readOnly 
+                    InputProps={{className: classes.root}}
+                    readOnly
                 />
             </Grid>
         </>           
